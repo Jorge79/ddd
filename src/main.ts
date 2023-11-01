34dbcfaf6fcfd81b4c87d54f91ed1c2769e08547
Jorge79 <Jorge7.9@hotmail.com>
2023-10-31 22:27:38 -0300

    change swc to tsc

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rio Vermelho", 1, "1111-111", "Bahia")
 customer.Address = address
 customer.activate()
 
-const item1 = new OrderItem("1", "item 1", 10)
-const item2 = new OrderItem("2", "item 2", 30)
+const item1 = new OrderItem("1", "item 1", 10, "1", 1)
+const item2 = new OrderItem("2", "item 2", 30, "2", 1)
 const order = new Order("1", "123", [item1, item2])
