e7ff871dc8a990a28eba36cf8560e9abb2974503
Jorge79 <Jorge7.9@hotmail.com>
2023-11-06 23:59:07 -0300

    fix name and imports

--- src/infrastructure/db/sequelize/model/order-item.model.ts
+++ src/infrastructure/db/sequelize/model/order-item.model.ts
@@ -4,26 +4,41 @@ import {
   PrimaryKey,
   Column,
   ForeignKey,
-  BelongsTo
+  BelongsTo,
 } from "sequelize-typescript";
-import CustomerModel from "./customer.model";
+import OrderModel from "./order-item.model";
+import ProductModel from "./product.model";
+
 
 @Table({
-  tableName: "order",
+  tableName: "order_items",
   timestamps: false,
 })
-export default class OrderModel extends Model {
+export default class OrderItemModel extends Model {
   @PrimaryKey
   @Column
   declare id: string;
 
-  @ForeignKey(() => CustomerModel)
+  @ForeignKey(() => ProductModel)
   @Column({ allowNull: false })
-  declare customer_id: string;
+  declare product_id: string;
 
-  @BelongsTo(() => CustomerModel)
-  declare customer: CustomerModel
+  @BelongsTo(() => ProductModel)
+  declare product: ProductModel;
+
+  @ForeignKey(() => OrderModel)
+  @Column({ allowNull: false })
+  declare order_id: string;
+
+  @BelongsTo(() => OrderModel)
+  declare order: OrderModel;
 
   @Column({ allowNull: false })
-  declare total: number
+  declare quantity: number;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare price: number;
 }
--- src/infrastructure/db/sequelize/model/order.model.ts
+++ src/infrastructure/db/sequelize/model/order.model.ts
@@ -4,41 +4,26 @@ import {
   PrimaryKey,
   Column,
   ForeignKey,
-  BelongsTo,
+  BelongsTo
 } from "sequelize-typescript";
-import OrderModel from "./order.model";
-import ProductModel from "./product.model";
-
+import CustomerModel from "./customer.model";
 
 @Table({
-  tableName: "order_items",
+  tableName: "order",
   timestamps: false,
 })
-export default class OrderItemModel extends Model {
+export default class OrderModel extends Model {
   @PrimaryKey
   @Column
   declare id: string;
 
-  @ForeignKey(() => ProductModel)
+  @ForeignKey(() => CustomerModel)
   @Column({ allowNull: false })
-  declare product_id: string;
+  declare customer_id: string;
 
-  @BelongsTo(() => ProductModel)
-  declare product: ProductModel;
-
-  @ForeignKey(() => OrderModel)
-  @Column({ allowNull: false })
-  declare order_id: string;
-
-  @BelongsTo(() => OrderModel)
-  declare order: OrderModel;
+  @BelongsTo(() => CustomerModel)
+  declare customer: CustomerModel
 
   @Column({ allowNull: false })
-  declare quantity: number;
-
-  @Column({ allowNull: false })
-  declare name: string;
-
-  @Column({ allowNull: false })
-  declare price: number;
+  declare total: number
 }
