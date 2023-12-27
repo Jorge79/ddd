add1991b1be1ddbc2cbf06661b209318dffa4835
Jorge79 <Jorge7.9@hotmail.com>
2023-12-27 13:43:41 -0300

    fix edited interface type

--- src/domain/@shared/customer/handler/customer-address.handler.ts
+++ src/domain/@shared/customer/handler/customer-address.handler.ts
@@ -1,8 +1,8 @@
-import CustomerCreatedEvent from "./customer-created.event";
 import EventHandlerInterface from "../../event-handler.interface";
 import EventInterface from "../../event.interface";
+import CustomerEditedEvent from "./customer-edited.event";
 
-export default class EditedCustomerHandler implements EventHandlerInterface<CustomerCreatedEvent> {
+export default class EditedCustomerHandler implements EventHandlerInterface<CustomerEditedEvent> {
   handle(event: EventInterface): void {
     const { eventData } = event
     console.log(`Cliente alterado para: id: ${eventData.id}, nome: ${eventData.name}, endereço: ${eventData.Address}`, event);
